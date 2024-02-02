5a1da8ce7124ebcbc435974b1e49a428cf538268
Mrskpatel <kasundrasarthi72@gmail.com>
2024-02-02 16:56:00 +0530

    Day-38

---
+++ Day-38/min.js
@@ -0,0 +1,13 @@
+function loadNav(){
+const nav = 
+    `<nav>
+    <a href="">Home</a>
+    <a href="">About us</a>
+    <a href="">Blogs</a>
+    <a href="">Shopes</a>
+    </nav>`
+
+    document.getElementById("navbar").innerHTML = nav
+}
+
+export{loadNav}
